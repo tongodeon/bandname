ef263c043707d2b72ce09ca08778490f6951f579
Laszlo Thoth <github@spam.tongodeon.com>
2015-04-21 15:01:20 +0100

    Comments for clarity. Minor refactoring.

--- bandname.py
+++ bandname.py
@@ -7,18 +7,18 @@ REASONABLE BAND NAME GENERATOR
 import random,string,sys
 
 prefix = ['b','bl','br',
-          'c','ch','cl','cr',
-          'd',
-          'f','fl','fn','fr',
-          'g','gh','gl','gr',
-          'h',
-          'l',
-          'm',
-          'n',
-          'p','pl',
-          't','tr',
-          'sc','scr','sh','sk','sp',
-          'w','wh','wr']
+		'c','ch','cl','cr',
+		'd',
+		'f','fl','fn','fr',
+		'g','gh','gl','gr',
+		'h',
+		'l',
+		'm',
+		'n',
+		'p','pl',
+		't','tr',
+		'sc','scr','sh','sk','sp',
+		'w','wh','wr']
 vowel = list("aeiou"*5+"y")
 mid = ['dder','ff','gg','n','nk','nt','mp','pp','pper','pple','ptas','rb','rd','rk','rn','rp','rt','st','tter']
 # suffixes that have all the vowels they need
@@ -30,16 +30,25 @@ suffix_adj = ['','','ing','able','apped','ed','ic']
 # non-pluralised suffixes for band names
 suffix_bn = ['','','er','erer','ic','in','unt','cunt','nut','nib','nip','nunt']
 
+# Generate 20 band names if no number is specified
 if len(sys.argv) > 1:
 	times = int(sys.argv[1])
 else:
 	times = 20
 
+# Generate the band names
 for i in xrange(times):
-	fn = string.capitalize(string.join(map(random.choice,[prefix,vowel,mid]+random.choice([[suffix],[vowel,vsuffix]])),''))
-	ln = string.capitalize(string.join(map(random.choice,[prefix,vowel,mid]+random.choice([[suffix],[vowel,vsuffix]])),''))
-	adj = string.capitalize(string.join(map(random.choice,[prefix,vowel,mid,suffix_adj]),''))
-	bn = string.capitalize(string.join(map(random.choice,[prefix,vowel,mid,suffix_bn]),'')+'s')
+	# first name
+	fn = map(random.choice,[prefix,vowel,mid]+random.choice([[suffix],[vowel,vsuffix]]))
+	# last name
+	ln = map(random.choice,[prefix,vowel,mid]+random.choice([[suffix],[vowel,vsuffix]]))
+	# adjective
+	adj = map(random.choice,[prefix,vowel,mid,suffix_adj])
+	# collective band name
+	bn = map(random.choice,[prefix,vowel,mid,suffix_bn])
+
+	fn,ln,adj,bn = map(lambda x:string.capitalize(string.join(x,'')), [fn,ln,adj,bn])
+	bn = bn+'s'
 
 	title = random.choice(['his','her','the','featuring'])
 	if title in ["featuring"]:
